eb395d2b1e57edcef84ac1e3f2a493876d334129
Martin Felke <martin.felke@googlemail.com>
2023-05-09 15:18:43 +0200

    pixelated clouds fix

--- src/shader_recompiler/backend/spirv/emit_spirv_context_get_set.cpp
+++ src/shader_recompiler/backend/spirv/emit_spirv_context_get_set.cpp
@@ -339,9 +339,10 @@ Id EmitGetAttribute(EmitContext& ctx, IR::Attribute attr, Id vertex) {
         if (ctx.profile.support_vertex_instance_id) {
             return ctx.OpBitcast(ctx.F32[1], ctx.OpLoad(ctx.U32[1], ctx.vertex_id));
         } else {
-            const Id index{ctx.OpLoad(ctx.U32[1], ctx.vertex_index)};
-            const Id base{ctx.OpLoad(ctx.U32[1], ctx.base_vertex)};
-            return ctx.OpBitcast(ctx.F32[1], ctx.OpISub(ctx.U32[1], index, base));
+            // const Id index{ctx.OpLoad(ctx.U32[1], ctx.vertex_index)};
+            // const Id base{ctx.OpLoad(ctx.U32[1], ctx.base_vertex)};
+            // return ctx.OpBitcast(ctx.F32[1], ctx.OpISub(ctx.U32[1], index, base));
+            return ctx.OpLoad(ctx.U32[1], ctx.vertex_index);
         }
     case IR::Attribute::BaseInstance:
         return ctx.OpBitcast(ctx.F32[1], ctx.OpLoad(ctx.U32[1], ctx.base_instance));
